500736424f72951e90e2f41e0ba5b7a599de853d
chaban <225617645+chaban-mb@users.noreply.github.com>
2026-09-13 09:41:21 +0200

    fix(harmony-enhancements): ignore alt-values comparison container in findTextNode

--- src/Harmony Enhancements.user.js
+++ src/Harmony Enhancements.user.js
@@ -2007,6 +2007,7 @@
                         return node;
                     }
                     if (node.nodeType === Node.ELEMENT_NODE) {
+                        if (node.matches('.alt-values')) continue;
                         const found = findTextNode(node, text);
                         if (found) return found;
                     }
